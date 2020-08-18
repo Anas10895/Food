518e6e6e89547bd0c5c6397c68992a4a9614912e
AnasShar <anas10895@gmail.com>
2020-08-18 11:43:26 +0300

    adding hooks

---
+++ hooks/useResultes.js
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react'
+import yelp from '../api/yelp'
+
+export default() => {
+    const [resultes , setResults] = useState([])
+    const [errorMessage , setErrorMessage] = useState('')
+
+
+    const serachApi = async(anything) => {
+        console.log('hi ')
+        try{
+            const response = await yelp.get('/search', {
+                params:{
+                    limit:33,
+                    term:anything,
+                    location: 'san jose', 
+     
+                }
+            });
+            setResults(response.data.businesses);
+        }catch(e){
+            setErrorMessage('Somthing went Wrong ya brother')
+        }
+
+    }
+    useEffect(() => {
+        serachApi('pasta')
+    }, [])
+    return [resultes, errorMessage, serachApi ]
+}
--- src/Screens/SerachScreen.js
+++ src/Screens/SerachScreen.js
@@ -1,32 +1,20 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import SerachBar from '../Components/SearchBar'
-import yelp from '../../api/yelp'
+import useResults from '../../hooks/useResultes'
 const SearchScreen = () => {
     const [serachTerm, setSearch] = useState('')
-    const [resultes , setResults] = useState([])
-
-    const serachApi = async() => {
-
-       const response = await yelp.get('/search', {
-           params:{
-               limit:50,
-               term:serachTerm,
-               location: 'san jose',
-
-           }
-       });
-       setResults(response.data.businesses);
-    }
+    const [resultes, errorMessage, serachApi ] = useResults();
     return <View>
 
     <SerachBar
     serachTerm={serachTerm}
     onSearchTermChange={newTerm => setSearch(newTerm)}
-    onTermSubmit={serachApi}
+    onTermSubmit={() => serachApi(serachTerm)}
     />
 
     <Text>Search Screen</Text>
+    {errorMessage ? <Text>{errorMessage}</Text> : <Text>Efry thing is fine ya brrother </Text>}
     {resultes.length ? <Text>We have found {resultes.length} resultes</Text> : <Text>no resultes</Text>}
     
 
